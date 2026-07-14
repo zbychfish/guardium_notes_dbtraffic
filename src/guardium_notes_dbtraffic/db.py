766049668464624f39af8989eba7d9fd484bb740
zbychfish <zszmigiero@gmail.com>
2026-07-14 23:41:53 +0200

    modified:   src/guardium_notes_dbtraffic/db.py

--- src/guardium_notes_dbtraffic/db.py
+++ src/guardium_notes_dbtraffic/db.py
@@ -204,32 +204,9 @@ class OracleAdapter(DatabaseAdapter):
 class InformixAdapter(DatabaseAdapter):
     name = "informix"
 
-    @staticmethod
-    def _preload_ifx_libraries() -> None:
-        import ctypes
-        import importlib.util
-        import os
-
-        spec = importlib.util.find_spec("IfxPy")
-        if not (spec and spec.origin):
-            return
-        site_packages = os.path.dirname(os.path.dirname(spec.origin))
-        driver_lib = os.path.join(site_packages, "onedb-odbc-driver", "lib")
-        load_order = [
-            os.path.join(driver_lib, "esql", "libifgls.so"),
-            os.path.join(driver_lib, "esql", "libifos.so"),
-            os.path.join(driver_lib, "esql", "libifgen.so"),
-            os.path.join(driver_lib, "cli", "libifcli.so"),
-            os.path.join(driver_lib, "cli", "libthcli.so"),
-        ]
-        for so in load_order:
-            if os.path.isfile(so):
-                ctypes.CDLL(so, mode=ctypes.RTLD_GLOBAL)
-
     def connect(self) -> None:
         if self.connection is not None:
             return
-        self._preload_ifx_libraries()
         import IfxPy
 
         server = self.config.database.server or self.config.database.database
